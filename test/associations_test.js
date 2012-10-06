bde063a34f43b1f62f55346d7b4ebd8e6a615164
Nicholas Faiz <nicholas.faiz@gmail.com>
2012-10-06 12:47:47 +1000

    more progres with belongsto"

--- test/associations_test.js
+++ test/associations_test.js
@@ -1,10 +1,14 @@
 var Downstairs = require('../lib/downstairs.js')
+  , Connection = Downstairs.Connection
   , should = require('should')
   , sql = require('sql')
   , env = require('./../config/env')
   , helper = require('./helper')
   , ectypes = helper.ectypes
-  , Table = Downstairs.Table;
+  , Table = Downstairs.Table
+  , faker2 = require('faker2');
+
+helper.resetConnection();
 
 var cheeseSchema = sql.Table.define({
       name: 'cheeses'
@@ -43,7 +47,7 @@ var mouseSQL = "CREATE TABLE mice\
 (\
   id bigserial NOT NULL,\
   cat_id bigserial NOT NULL,\
-  micename character varying(100) unique NOT NULL,\
+  name character varying(100) unique NOT NULL,\
   created_at timestamp with time zone NOT NULL DEFAULT now(),\
   updated_at timestamp with time zone NOT NULL DEFAULT now(),\
   CONSTRAINT pk_mice PRIMARY KEY (id)\
@@ -53,13 +57,13 @@ var catSchema = sql.Table.define({
       name: 'cats'
       , quote: true
       , columns: ['id' 
-        , 'catname' 
+        , 'name' 
         , 'created_at'
         , 'updated_at'
       ]
     });
 
-var catTableSQL = "CREATE TABLE cats\
+var catSQL = "CREATE TABLE cats\
 (\
   id bigserial NOT NULL,\
   name character varying(100) unique NOT NULL,\
@@ -70,45 +74,54 @@ var catTableSQL = "CREATE TABLE cats\
 
 var Cat = Table.model(catSchema);
 
-Cheese.belongsTo(Mouse, {foreignKey: 'mouse_id', eager: true});
-Mouse.hasOne(Cheese);
-Mouse.belongsTo(Cat, {foreignKey: 'cat_id'});
-Cat.hasMany(Mouse);
+Cheese.belongsTo(Mouse, {foreignKey: 'mouse_id', as: 'cheese', eager: true});
+Mouse.hasOne(Cheese, {as: 'mouse'});
+Mouse.belongsTo(Cat, {foreignKey: 'cat_id', as: 'cat'});
+Cat.hasMany(Mouse, {as: 'mice'});
 
-var associationsBlueprint =
-  {
-    Cheese: { 
-      name: function(){ return faker2.Internet.email() } 
-    }
-    , Mouse: { 
-      name: function(){ return faker2.Internet.email() } 
-    }
-    , Cat: { 
-      name: function(){ return faker2.Internet.email() } 
-    } 
-}
+var associationsBlueprint = [
+  {Cheese: { 
+      name: function(){ return faker2.Lorem.words(2).join(' ') } 
+    }}
+    , {Mouse: { 
+      name: function(){ return faker2.Lorem.words(2).join(' ') } 
+    }}
+    , {Cat: { 
+      name: function(){ return faker2.Lorem.words(2).join(' ') } 
+    }}]
+
+ectypes.add(associationsBlueprint);
 
 describe('setting up associations', function(){
-  var cheese, mouse1, mouse2, cat;
-
   beforeEach(function(done){
-    helper.resetDb(mouseSQL + catTableSQL + cheeseSQL, done);
+    helper.resetDb(mouseSQL + catSQL + cheeseSQL, done);
   })
 
   beforeEach(function(done){
-    ectypes.Cheese.create(function(err, cheese){
-      cheese = cheese;
+    ectypes.Cat.create(function(err, result){
+      ectypes.Mouse.create(function(err, result){
+        ectypes.Cheese.create(function(err, result){
+          done();
+        })
+      })
     })
   })
 
+  // beforeEach(function(done){
+  //   var cheese, mouse1, mouse2, cat;
+  //   Cat.findAll(function(err, cats){
+  //     cat = cats[0];
+  //     done();
+  //   })
+  // })
+
   it('finds an associated model via belongsTo', function(done){
-    console.log(cheese, " <<< cheese");
-    
+    Cheese.findAll(function(err, cheeses){
+      var cheese = cheeses[0];
+      console.log(cheese, " <<< cheese");
+      should.exist(cheese.mouse);
+      done();
+    })
   })
 
 });
-
-
-
-
-
